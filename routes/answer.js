1b7975f9c3cd1c30ab2e3a14288075b2a9311141
sami-aburwaise <tarrary1@gmail.com>
2023-11-08 14:23:05 +0300

    create routes and comtroller files

---
+++ routes/answer.js
@@ -0,0 +1,14 @@
+//  load packages
+const express = require('express')
+
+//  invoke express router functionality
+const router = express.Router()
+
+//  controllers
+const userCtrl = require('../controllers/answer')
+
+//  routers
+
+
+//  expoert router to server
+module.exports = router
